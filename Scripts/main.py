0a08841afce03acd0f5cb9ab1b852bfb14d2c78a
nguyenVux <rockmanx000@gmail.com>
2022-04-02 22:04:01 +0700

    init scripts

---
+++ Scripts/main.py
@@ -0,0 +1,19 @@
+
+import os, pickle, shutil
+import numpy as np
+from skimage.io import imread
+import skimage.morphology as morp
+from skimage.filters import rank
+from sklearn.utils import shuffle, compute_class_weight
+from sklearn.metrics import confusion_matrix
+import csv
+import cv2
+import matplotlib.pyplot as plt
+from keras.models import Input, Model
+from keras.layers import Conv2D, MaxPooling2D, Dropout, Flatten, Dense
+from tensorflow.keras.utils import to_categorical
+from keras import optimizers
+from keras.initializers import random_normal
+from keras.callbacks import Callback, ReduceLROnPlateau, ModelCheckpoint
+import seaborn as sn
+from sklearn.metrics import confusion_matrix
